96f3925ffa9d54b2462956387124e5fbd1ab8b6c
bryle99 <brylebaran@gmail.com>
2022-08-16 08:19:54 +0800

    server config

--- server/config/express.js
+++ server/config/express.js
@@ -1,13 +1,14 @@
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
+const config = require('./config.json');
 
 const app = express();
 
 require('dotenv').config();
 
-app.set('port', process.env.APP_PORT || 5000);
-app.set('host', process.env.APP_HOST || 'localhost');
+app.set('port', config['app-port'] || 5000);
+app.set('host', config['app-host'] || 'localhost');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
